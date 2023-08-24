a11015f045b77896c91bf577be83ac954a2f7ab0
Sravan Kairamkonda <35738653+SravanKairamkonda@users.noreply.github.com>
2023-08-24 19:49:30 +0100

    Throwing finished
    Increasing the velocity scale can  throw an object to far distance

---
+++ Assets/Scripts/DestroyEnemy.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyEnemy : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Enemy"))
+        {
+            Destroy(collision.gameObject, 1f);
+            Destroy(this.gameObject);
+        }
+    }
+}
--- Assets/Scripts/EnemyNavMesh.cs
+++ Assets/Scripts/EnemyNavMesh.cs
@@ -6,21 +6,38 @@ using UnityEngine.AI;
 public class EnemyNavMesh : MonoBehaviour
 {
     private NavMeshAgent navMeshAgent;
+    private Animator getAnimator;
+
     [SerializeField]
     private Transform target;
 
     private void Awake()
     {
         navMeshAgent = GetComponent<NavMeshAgent>();
-        Debug.Log(navMeshAgent.speed);
         navMeshAgent.speed /= 2.5f;
+
     }
 
-    
     void Update()
     {
-        navMeshAgent.destination = target.position;
+        MoveAgent();
+        
     }
 
+    void MoveAgent()
+    {
+        navMeshAgent.destination = target.position;
 
+        
+    }
+
+    void PlayIdleAnimation()
+    {
+        getAnimator.Play("Mutant Idle");
+    }
+
+    void PlayWalkAnimation()
+    {
+        getAnimator.Play("Mutant Walking");
+    }
 }
