68c859656b7626e2e7252d1323049d1a5dc308d8
Sravan Kairamkonda <35738653+SravanKairamkonda@users.noreply.github.com>
2023-08-24 15:05:47 +0100

    Enemy Mutant is moving on the path
    Need to add different states to idle and walk

---
+++ Assets/Scripts/EnemyNavMesh.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyNavMesh : MonoBehaviour
+{
+    private NavMeshAgent navMeshAgent;
+    [SerializeField]
+    private Transform target;
+
+    private void Awake()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        Debug.Log(navMeshAgent.speed);
+        navMeshAgent.speed /= 2.5f;
+    }
+
+    
+    void Update()
+    {
+        navMeshAgent.destination = target.position;
+    }
+
+
+}
