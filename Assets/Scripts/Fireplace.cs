679bfaccb12d83305eccc627cd042960cb742b3a
Sravan Kairamkonda <35738653+SravanKairamkonda@users.noreply.github.com>
2023-08-29 16:39:41 +0100

    Fire respawn after destroying

---
+++ Assets/Scripts/Fireplace.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fireplace : MonoBehaviour
+{
+    public GameObject firePrefab;
+    private Vector3 spawnPosition;
+    private GameObject fireHolder;
+
+    private void Awake()
+    {
+        spawnPosition = transform.position + Vector3.up * 2;
+    }
+
+    private void Update()
+    {
+        SpawnPrefab();
+    }
+
+    public void SpawnPrefab()
+    {
+        if (fireHolder == null)
+        {
+            GameObject temp = Instantiate(firePrefab, spawnPosition, transform.rotation);
+            //temp.gameObject.transform.SetParent(this.transform);
+            fireHolder = temp;
+        }
+    }
+
+
+
+}
